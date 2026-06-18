e50a24e6e336c79e628a63914161f1a41bf7c239
predatorx7 <mushaheed.syed@proton.me>
2026-06-18 23:22:54 +0530

    Add readline for sessionId

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
 import { verifySession } from "./validate"
+import readline from "readline/promises";
 
 const main = async () => {
     try {
-        const response = await verifySession("99f86066d7", {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        const sessionId = await rl.question('Enter session Id: ');
+
+        if (!sessionId) {
+            throw new Error('No session ID provided');
+        }
+
+        const response = await verifySession(sessionId, {
             verificationConfig: {
                 teeAttestation: undefined,
                 attestorTeeAttestation: undefined
             }
         });
-        console.info({ response: JSON.stringify(response, null, 2) });
+        console.info(JSON.stringify(response, null, 2));
+
     } catch (error) {
         console.error({ error });
     }
